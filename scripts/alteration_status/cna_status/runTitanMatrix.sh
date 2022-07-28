553b94761a29ff9ae7fa066ebda819f32a6c4dde
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-07-27 23:10:19 -0400

    Update runTitanMatrix.sh

--- scripts/alteration_status/cna_status/runTitanMatrix.sh
+++ scripts/alteration_status/cna_status/runTitanMatrix.sh
@@ -1,1 +1,1 @@
-Rscript makeMatrixFromTITAN-ICHOR_segBased_hg38.R path_to_optimalClusterSolution/ ../../../metadata/GRCh38.p12.ensembl.gene.annotations.sorted.include.AREnhancer.txt ../../../metadata/sampleList_allCases.txt common Gene 1000 CRPC10X_WCDT
+Rscript makeMatrixFromTITAN-ICHOR_segBased_hg38.R /home/ma1111/tools/TitanCNA_SV_WGS/results/titan/hmm/optimalClusterSolution/ ../../../metadata/GRCh38.p12.ensembl.gene.annotations.sorted.include.AREnhancer.txt ../../../metadata/sampleList_tRCC.txt common Gene 1000 DTRCC4
